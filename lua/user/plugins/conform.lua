5ae151ad6f694bed4bafdac80d8f39edaffc970f
Chris Gatewood <chris.gatewood@talkdesk.com>
2025-11-07 14:17:46 -0800

    Fix conform plugin (was not formatting for other code formats related to IaC)

--- lua/user/plugins/conform.lua
+++ lua/user/plugins/conform.lua
@@ -36,7 +36,18 @@ return { -- Autoformat
       --
       -- You can use 'stop_after_first' to run the first available formatter from the list
       -- javascript = { "prettierd", "prettier", stop_after_first = true },
+      
+      -- DevOps & Infrastructure-as-Code
+      terraform = { "terraform_fmt" },
+      tf = { "terraform_fmt" },
+      hcl = { "terraform_fmt" },
       yaml = { "yamlfmt" },
+      json = { "jq" },
+      jsonc = { "jq" },
+      markdown = { "prettier" },
+      sh = { "shfmt" },
+      bash = { "shfmt" },
+      toml = { "taplo" },
     },
     formatters = {
       yamlfmt = {
